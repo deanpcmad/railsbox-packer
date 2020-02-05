6f6a09d4076c064d5417d874bb0fa16d15b1921d
Dean Perry <dean@deanpcmad.com>
2020-02-05 20:20:19 +0000

    ruby1.9 & ruby2.7

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-# Railsbox Packer
-
-[![Build Status](https://travis-ci.org/joshfng/railsbox-packer.svg?branch=master)](https://travis-ci.org/joshfng/railsbox-packer)
+# Railsbox - Ruby1.9 & Ruby2.7
 
 Scripts used for building [railsbox](https://github.com/joshfng/railsbox). If you're looking for just the Vagrant box to run a local Ruby/Rails dev env please see the [railsbox repo](https://github.com/joshfng/railsbox).
 
 You can find the box hosted on Atlas [here](https://atlas.hashicorp.com/joshfng/boxes/railsbox/)
+
+## Ruby1.9
+
+Uses a Ubuntu 16.04 machine & installs Ruby 1.9.3-p511 with MariaDB 10.1
+
+# Ruby2.7
+
+Uses a Ubuntu 18.04 machine and installs Ruby 2.7.0 with MariaDB 10.4
 
 ## Development
 All the work is done in `scripts/provision.sh`, modify it to suite your needs.
--- ruby_19/provision.sh
+++ ruby_19/provision.sh
@@ -1,30 +1,28 @@
 #!/usr/bin/env bash
 
 export DEBIAN_FRONTEND=noninteractive
-export RUBY_VERSION=2.7.0
+export RUBY_VERSION=1.9.3-p551
 
 # Update Package List
-
 apt-get update
 
-# Update System Packages
-apt-get -y dist-upgrade
+# Install Kernel Headers
+apt-get install -y linux-headers-$(uname -r) build-essential curl zip unzip
+
+# Upgrade System Packages
+apt-get dist-upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" --force-yes
 
 # Force Locale
-
 echo "LC_ALL=en_US.UTF-8" >> /etc/default/locale
 locale-gen en_US.UTF-8
 
 # Install Some PPAs
-
 apt-get install -y software-properties-common curl
 
 # Update Package Lists
-
 apt-get update
 
 # Install Some Basic Packages
-
 apt-get install -y build-essential dos2unix gcc git libmcrypt4 libpcre3-dev \
 make python2.7-dev python-pip re2c unattended-upgrades whois vim \
 libnotify-bin git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev \
@@ -32,20 +30,12 @@ libyaml-dev libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-proper
 libffi-dev
 
 # Set Timezone
+ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
 
-ln -sf /usr/share/zoneinfo/UTC /etc/localtime
-
-# Install Node and NPM
-su -l -c 'touch ~/.bash_profile' vagrant
-su -l -c 'export PROFILE=~/.bash_profile && curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash' vagrant
-su -l -c 'source ~/.bash_profile' vagrant
-su -l -c 'nvm install node' vagrant
-su -l -c 'node --version' vagrant
-su -l -c 'npm --version' vagrant
-su -l -c 'npm install -g yarn' vagrant
+# Install Node
+apt install -y nodejs
 
 # Install Ruby
-
 su -l -c 'git clone https://github.com/sstephenson/rbenv.git ~/.rbenv' vagrant
 su -l -c 'git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build' vagrant
 su -l -c 'git clone https://github.com/sstephenson/rbenv-vars.git ~/.rbenv/plugins/rbenv-vars' vagrant
@@ -55,28 +45,19 @@ su -l -c 'cd ~/.rbenv && src/configure && make -C src' vagrant
 su -l -c 'touch ~/.bash_profile' vagrant
 su -l -c "echo 'export PATH=\"\$HOME/.rbenv/bin:\$PATH\"' >> ~/.bash_profile" vagrant
 su -l -c "echo 'eval \"\$(rbenv init -)\"' >> ~/.bash_profile" vagrant
-su -l -c "rbenv install $RUBY_VERSION && rbenv rehash" vagrant
-su -l -c "rbenv global $RUBY_VERSION && rbenv rehash" vagrant
+su -l -c "rbenv install $RUBY_VERSION" vagrant
+su -l -c "rbenv global $RUBY_VERSION" vagrant
 
-su -l -c 'gem update --system' vagrant
-su -l -c 'gem install bundler rails --no-document' vagrant
+su -l -c 'gem install bundler -v 1.17.3 --no-document' vagrant
+su -l -c "rbenv rehash" vagrant
 
-# Install Postgres
-
-apt-get install -y postgresql libpq-dev
-
-# allow vagrant user to create databases
-sed -i 's/md5/trust/g' /etc/postgresql/10/main/pg_hba.conf
-sudo -u postgres createuser --superuser --createdb vagrant
-ls /etc/postgresql
-echo "listen_addresses = '*'" >> /etc/postgresql/10/main/postgresql.conf
-
-service postgresql restart
+# Install MariDB
+apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+add-apt-repository -y 'deb [arch=amd64,arm64,i386,ppc64el] http://mirrors.coreix.net/mariadb/repo/10.1/ubuntu xenial main'
+apt install -y mariadb-server mariadb-client libmysqlclient-dev
 
 # Install Redis
-
-apt-get install -y redis
-systemctl enable redis-server
+apt-get install -y redis-server
 
 su -l -c "echo 'gem: --no-document' >> ~/.gemrc" vagrant
 su -l -c "echo 'cd /vagrant' >> ~/.bash_profile" vagrant
